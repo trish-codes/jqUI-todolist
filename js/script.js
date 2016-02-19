72d5b5cb7d52047af85fe247542d0ad1e628df90
trish <trish.codes@gmail.com>
2016-02-19 12:29:59 -0600

    fixed delete placeholder

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 $(function() {
-    
+      
     $('#todoList ul').sortable({
         items: "li:not('.listTitle, .addItem')",
         connectWith: "ul",
         dropOnEmpty: true,
-        placeholder: "emtySpace"
+        placeholder: "emptySpace"
     });
     
-    $('input').keydown(function(e){
-        if(e.keyCode == 13){
+    $('input').keydown(function(e) {
+        if(e.keyCode == 13) {
             var item = $(this).val();
             
             $(this).parent().parent().append('<li>' + item + '</li>');
@@ -17,8 +17,9 @@ $(function() {
     });
     
     $('#trash').droppable({
-        drop: function(event, ui){
-            ui.draggble.remove();
-    }
+        drop: function(event, ui) {
+            ui.draggable.remove();
+        }
     });
+    
 });
