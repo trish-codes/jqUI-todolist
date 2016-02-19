da6b88ee68644015baff4b69ec692b36bc1ef9b6
trish <trish.codes@gmail.com>
2016-02-19 12:15:26 -0600

    item can now be added to list, also draggable

--- js/script.js
+++ js/script.js
@@ -6,4 +6,15 @@ $(function() {
         dropOnEmpty: true,
         placeholder: "emtySpace"
     });
+    
+    $('input').keydown(function(e){
+        if(e.keyCode == 13){
+            var item = $(this).val();
+            
+            $(this).parent().parent().append('<li>' + item + '</li>');
+        }
+        
+        
+        
+    });
 });
